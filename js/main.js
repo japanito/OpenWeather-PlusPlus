0a72377a9d7443aa03f86284edf294922a95ec91
Tyler Hoeflicker <tyler.hoeflicker@gmail.com>
2012-09-18 17:53:38 -0500

    Updates to Build Guide. Added cloud animation to landing page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-
+	var hiddenCloud = $('<img id="hiddenCloud2" src="images/cloud-hidden2.png"></img>').css({'opacity' : 0});
+	
+	setTimeout(function(){
+		$('.container-fluid').append(hiddenCloud);
+		hiddenCloud.addClass('animated fadeInLeft');
+	}, 4000);
+	
 });
